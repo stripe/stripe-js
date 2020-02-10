3102ef29dcd29a74d791c2327696ba68168672b2
Christopher Henn <christopher@stripe.com>
2020-02-10 13:51:38 -0800

    Fix test

--- tests/types/index.ts
+++ tests/types/index.ts
@@ -132,6 +132,13 @@ const paymentRequestButtonElement = elements.create('paymentRequestButton', {
       theme: 'light',
     },
   },
+  paymentRequest: stripe.paymentRequest({
+    country: 'US',
+    currency: 'usd',
+    total: {label: 'Demo total', amount: 1000},
+    requestPayerName: true,
+    requestPayerEmail: true,
+  }),
 });
 
 const retrievedPaymentRequestButtonElement: StripePaymentRequestButtonElement | null = elements.getElement(
