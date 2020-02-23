3c9e2f6a4f881e56dd3f58ae6d809825a42cb352
Mathieu Hofman <hofman@stripe.com>
2020-02-23 09:51:37 -0800

    Test that implementation and module type definitions match (#12)

--- tests/types/index.ts
+++ tests/types/index.ts
@@ -39,6 +39,15 @@ import {
 const stripePromise: Promise<Stripe | null> = loadStripe('');
 const stripeConnectPromise = loadStripe('', {stripeAccount: '', locale: 'en'});
 
+type TypeModule = typeof import('@stripe/stripe-js');
+type SrcModule = typeof import('../../src/index');
+
+// Makes sure that the implementation matches the type definitions
+// Checking for compatibility both ways ensures that the exports
+// are equivalent with nothing missing on either side.
+import('../../src/index').then((srcModule: TypeModule) => {});
+import('@stripe/stripe-js').then((typeModule: SrcModule) => {});
+
 declare const stripe: Stripe;
 
 const OPEN_SANS: CssFontSource = {
