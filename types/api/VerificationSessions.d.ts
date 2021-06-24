f0a78197af6cba6184c6db5e1456e88dc01e0757
BZ (Brian Zhang) <bz@stripe.com>
2021-06-24 12:47:13 -0700

    add typing for stripe.verifyIdentity

---
+++ types/api/VerificationSessions.d.ts
@@ -0,0 +1,11 @@
+declare module '@stripe/stripe-js' {
+  /**
+   * The VerificationSession object.
+   */
+  interface VerificationSession {
+    /**
+     * Unique identifier for the object.
+     */
+    id: string;
+  }
+}
