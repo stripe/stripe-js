ba8685e91584c74aeb056d086a7b6cee63ff0cde
Tony Pianta <tpianta@stripe.com>
2022-08-30 08:58:37 -0400

    move radar types to their own file

--- types/stripe-js/index.d.ts
+++ types/stripe-js/index.d.ts
@@ -3,6 +3,7 @@ export * from './elements';
 export * from './elements-group';
 export * from './payment-intents';
 export * from './payment-request';
+export * from './radar';
 export * from './setup-intents';
 export * from './token-and-sources';
 export * from './stripe';
