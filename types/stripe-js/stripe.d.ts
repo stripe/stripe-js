5e33ebe44931dc2647f348ce3d480f9d5e199f59
Tony Pianta <tpianta@stripe.com>
2022-09-08 18:09:41 -0400

    prettier

--- types/stripe-js/stripe.d.ts
+++ types/stripe-js/stripe.d.ts
@@ -1060,11 +1060,11 @@ export type CollectBankAccountTokenResult =
       error: StripeError;
     };
 
-  /* A Radar Session is a snapshot of the browser metadata and device details that helps Radar make more accurate predictions on your payments. 
+/* A Radar Session is a snapshot of the browser metadata and device details that helps Radar make more accurate predictions on your payments. 
   This metadata includes information like IP address, browser, screen or device information, and other device characteristics. 
   You can find more details about how Radar uses this data by reading about how Radar performs [advanced fraud detection](https://stripe.com/docs/disputes/prevention/advanced-fraud-detection).
   */
-  export type RadarSessionPayload =
+export type RadarSessionPayload =
   | {radarSession: Record<any, any>; error?: undefined}
   | {radarSession?: undefined; error: StripeError};
 
