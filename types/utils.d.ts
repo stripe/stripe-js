c0ae5f50c8f6754cb07aaf494d90ca4c06931358
Yevhen Yerko <osben90@gmail.com>
2023-10-18 09:37:54 +0300

    fix: Polyfill
    works with 2.6

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -1,2 +1,3 @@
 // Polyfill for TypeScript < 3.5 compatibility
-export type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T]
+export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
