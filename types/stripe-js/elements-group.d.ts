03fce8f2d42667d2de8777778e63ff36853b7068
Andrew Rojas <amrojas@stripe.com>
2024-01-02 14:03:11 -0800

    Add external payment method types

--- types/stripe-js/elements-group.d.ts
+++ types/stripe-js/elements-group.d.ts
@@ -796,6 +796,20 @@ export interface StripeElementsOptionsMode extends BaseStripeElementsOptions {
    * Either use mode or clientSecret when creating an Elements group
    */
   clientSecret?: never;
+
+  /**
+   * The external payment methods to be displayed in the Payment Element that you are already integrated with.
+   * 
+   * @docs https://stripe.com/docs/js/elements_object/create#stripe_elements-options-externalPaymentMethodTypes
+   */
+  externalPaymentMethodTypes? : string;
+
+  /**
+   * The external payment methods to be displayed in the Payment Element that you are already integrated with.
+   * 
+   * @docs https://stripe.com/docs/js/elements_object/create#stripe_elements-options-externalPaymentMethodTypes
+   */
+  external_payment_method_types? : string;
 }
 
 export type StripeElementsOptions =
