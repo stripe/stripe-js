233cfea72343068aaeb82ae8efc31ab5c0c934d6
Robert Fruchtman <fruchtose@stripe.com>
2024-02-13 14:05:58 -0800

    Fix types for exports (#542, #543, #545)
    See: https://github.com/microsoft/TypeScript/issues/52363#issuecomment-1659179354

--- types/pure.d.ts
+++ types/pure.d.ts
@@ -1,4 +1,4 @@
-///<reference path='./types/index.d.ts' />
+///<reference path='./index.d.ts' />
 
 export const loadStripe: typeof import('@stripe/stripe-js').loadStripe & {
   setLoadParameters: (params: {advancedFraudSignals: boolean}) => void;
