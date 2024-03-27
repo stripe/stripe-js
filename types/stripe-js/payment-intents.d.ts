9ea9b39e158f9c2e6ba143eabf972925b5f72960
James Arment <jima@stripe.com>
2024-03-27 07:33:36 -0700

    Add allow_redisplay parameter to ConfirmationTokenCreateParams and ConfirmPaymentData

--- types/stripe-js/payment-intents.d.ts
+++ types/stripe-js/payment-intents.d.ts
@@ -1424,6 +1424,9 @@ export interface ConfirmPaymentData extends PaymentIntentConfirmParams {
     billing_details?: PaymentMethodCreateParams.BillingDetails;
 
     /**
+     * Requires beta access:
+     * Contact [Stripe support](https://support.stripe.com/) for more information.
+     * 
      * Specifies if the PaymentMethod should be redisplayed when using the Saved Payment Method feature
      */
     allow_redisplay?: 'always' | 'limited' | 'unspecified';
