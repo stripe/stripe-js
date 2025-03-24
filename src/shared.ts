d83024778a596e5dbfd4d37deaa539c0622dbbdc
dodgez-stripe <dodgez@stripe.com>
2025-03-24 10:08:18 -0700

    Setup basil release candidate (#740)

--- src/shared.ts
+++ src/shared.ts
@@ -12,7 +12,7 @@ export interface LoadParams {
 // containing the package.json version
 declare const _VERSION: string;
 
-export const RELEASE_TRAIN = 'acacia';
+export const RELEASE_TRAIN = 'basil';
 
 const runtimeVersionToUrlVersion = (version: string | number) =>
   version === 3 ? 'v3' : version;
